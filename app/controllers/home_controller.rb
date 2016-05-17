984c9bf2e6f325f1054772ebc18513ebe9b5de55
Greta Gangle <greta.gangle@gmail.com>
2016-05-17 13:46:07 -0700

    First commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,44 @@
+class HomeController < ApplicationController
+
+	def home
+
+		response = HTTParty.get("https://#{ENV['BASE_DOMAIN']}.pagerduty.com/api/v1/escalation_policies/on_call", {
+			headers: {
+				"Authorization" => "Token token=#{ENV["PAGERDUTY_TOKEN"]}",
+				},
+			format: :json,
+		})
+
+		@collected = {}
+
+		ids_to_titles = {
+			"#{ENV["CA_POLICY"]}" => "CA/UW",
+			"#{ENV["CS_POLICY"]}" => "CS",
+			"#{ENV["DEVOPS_POLICY"]}" => "DevOps",
+			"#{ENV["IT_POLICY"]}" => "IT",
+			"#{ENV["OPS_POLICY"]}" => "Ops",
+			"#{ENV["PM_POLICY"]}" => "PM",
+		}
+	
+		response["escalation_policies"].each do |policy|
+			id = policy["id"]
+			on_call_users = policy["on_call"]
+		
+
+			if ["#{ENV["CA_POLICY"]}", "#{ENV["CS_POLICY"]}"].include? id
+				@collected[id] = {
+				title: ids_to_titles[id],
+				primary: on_call_users.second,
+				secondary: on_call_users.third,
+			}
+			else
+				@collected[id] = {
+					title: ids_to_titles[id],
+					primary: on_call_users.first,
+					secondary: on_call_users.second,
+				}
+			end
+		end
+	end
+
+end
