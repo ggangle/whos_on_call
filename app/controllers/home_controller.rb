fb8e745bc100a3a7ae06264c7e01227b4e779233
Greta Gangle <greta.gangle@gmail.com>
2016-05-25 14:18:37 -0700

    Add phone number

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
-
 	def home
-
 		response = HTTParty.get("https://#{ENV['BASE_DOMAIN']}.pagerduty.com/api/v1/escalation_policies/on_call", {
 			headers: {
 				"Authorization" => "Token token=#{ENV["PAGERDUTY_TOKEN"]}",
@@ -9,7 +7,7 @@ class HomeController < ApplicationController
 			format: :json,
 		})
 
-		@collected = {}
+		@user_records = {}
 
 		ids_to_titles = {
 			"#{ENV["CA_POLICY"]}" => "CA/UW",
@@ -24,35 +22,43 @@ class HomeController < ApplicationController
 			id = policy["id"]
 			on_call_users = policy["on_call"]
 
+			primary_user = nil
+			secondary_user = nil
+
 			if ["#{ENV["CA_POLICY"]}", "#{ENV["CS_POLICY"]}"].include? id
-				@collected[id] = {
-				title: ids_to_titles[id],
-				primary: on_call_users.second,
-				secondary: on_call_users.third,
-			}
+				primary_user = on_call_users.second
+				secondary_user = on_call_users.third
 			else
-				@collected[id] = {
-					title: ids_to_titles[id],
-					primary: on_call_users.first,
-					secondary: on_call_users.second,
-				}
+				primary_user = on_call_users.first
+				secondary_user = on_call_users.second
 			end
 
+			@user_records[id] = {
+				title: ids_to_titles[id],
+				primary: primary_user,
+				secondary: secondary_user,
+				primary_phone: get_phone(primary_user),
+				# secondary_phone: phone2,
+			}
+
 		end
-
-		# user_response = HTTParty.get("https://#{ENV['BASE_DOMAIN']}.pagerduty.com/api/v1/users/#{user_id}/contact_methods", {
-		# 		headers: {
-		# 				"Authorization" => "Token token=#{ENV["PAGERDUTY_TOKEN"]}",
-		# 		},
-		# 		format: :json,
-		# })
-    #
-		# user_response.each do |contact|
-    #
-		# 				@phone = contact[1]["phone_number"]
-    #
-		# end
-
 	end
 
+  def get_phone(user)
+		user_id = user["user"]["id"]
+
+		response = HTTParty.get("https://#{ENV['BASE_DOMAIN']}.pagerduty.com/api/v1/users/#{user_id}/contact_methods", {
+				headers: {
+						"Authorization" => "Token token=#{ENV["PAGERDUTY_TOKEN"]}",
+				},
+				format: :json,
+		})
+
+		phone_number = nil
+
+		response["contact_methods"].each do |contact_method|
+			phone_number = contact_method["phone_number"] if contact_method.has_key?("phone_number")
+		end
+		phone_number
+	end
 end
